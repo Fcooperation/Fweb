a5a902cded0de2354c68943301b80bfa89f710f3
Fcooperation <thefcooperation@gmail.com>
2026-05-31 21:34:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,12 @@ const menuBtn = document.getElementById("menuBtn");
 const dropdown = document.getElementById("dropdown");
 
 menuBtn.addEventListener("click", () => {
-  if (dropdown.style.display === "flex") {
-    dropdown.style.display = "none";
-  } else {
-    dropdown.style.display = "flex";
-  }
+  dropdown.classList.toggle("show");
 });
 
 // close menu when clicking outside
 document.addEventListener("click", (e) => {
   if (!menuBtn.contains(e.target) && !dropdown.contains(e.target)) {
-    dropdown.style.display = "none";
+    dropdown.classList.remove("show");
   }
 });
